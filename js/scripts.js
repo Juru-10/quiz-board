20463e56e78ba26f1694b4e475df03720669aa62
JURU <jurassu10@gmail.com>
2018-12-21 14:57:30 +0200

    add the back end scripts part

--- js/scripts.js
+++ js/scripts.js
@@ -11,32 +11,3 @@ ansrs[6]="myFunction()";
 ansrs[7]="if (i == 5)";
 ansrs[8]="if (i != 5)";
 ansrs[9]="while (i <= 10)";
-
-$(getScore).ready(function() {
-  $("#getScore").onclick(function (event){
-
-var score = 0;
-var currElt;
-var currSelection;
-for (i=0; i<numQues; i++) {
-currElt = i*numChoi;
-answered=false;
-for (j=0; j<numChoi; j++) {
-currSelection = form.elements[currElt + j];
-if (currSelection.checked) {
- answered=true;
- if (currSelection.value == ansrs[i]) {
-   score++;
-   break;
- }
-}
-}
-if (answered ===false){alert("Do answer all the questions, Please") ;return false;}
-}
-
-var scoreper = Math.round(score/numQues*100);
-form.percentage.value = scoreper + "%";
-form.mark.value=score;
-
-
-});});
