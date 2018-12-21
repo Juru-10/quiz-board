36bbcd8ee0d8120483101f61efa717373bb952e3
JURU <jurassu10@gmail.com>
2018-12-21 17:40:04 +0200

    add jqueries

--- js/scripts.js
+++ js/scripts.js
@@ -2,41 +2,42 @@ var numQues = 10;
 var numChoi = 4;
 var ansrs = new Array(10);
 ansrs[0]="&ltscript&gt";
-
+ansrs[1]="Node based server uses a single threaded model and can serve much larger number of requests compared to any traditional server like Apache HTTP Server.";
 ansrs[2]="Both the &lthead&gt section and the &ltbody&gt section are correct";
 ansrs[3]="External";
-
+ansrs[4]="Both of the above.";
 ansrs[5]="function myFunction()";
 ansrs[6]="myFunction()";
 ansrs[7]="if (i == 5)";
 ansrs[8]="if (i != 5)";
 ansrs[9]="while (i <= 10)";
 
-$(getScore).ready(function() {
-  $("#getScore").onclick(function (event){
-    var score = 0;
-    var currElt;
-    var currSelection;
-    for (i=0; i<numQues; i++) {
-      currElt = i*numChoi;
-      answered=false;
-      for (j=0; j<numChoi; j++) {
-        currSelection = form.elements[currElt + j];
-        if (currSelection.checked) {
-          answered=true;
-          if (currSelection.value == ansrs[i]) {
-            score++;
-            break;
-          }
+function getScore(form) {
+
+  var score = 0;
+  var currElt;
+  var currSelection;
+  for (i=0; i<numQues; i++) {
+    currElt = i*numChoi;
+    answered=false;
+    for (j=0; j<numChoi; j++) {
+      currSelection = form.elements[currElt + j];
+      if (currSelection.checked) {
+        answered=true;
+        if (currSelection.value == ansrs[i]) {
+          score++;
+          break;
         }
       }
-      if (answered ===false){alert("Do answer all the questions, Please") ;return false;}
     }
+    if (answered ===false){alert("Do answer all the questions, Please") ;return false;}
+  }
 
-    var scoreper = Math.round(score/numQues*100);
-    form.percentage.value = scoreper + "%";
-    form.mark.value=score;
+  var scoreper = Math.round(score/numQues*100);
+  form.percentage.value = scoreper + "%";
+  form.mark.value=score;
 
+    $(".col-md-6,#grd,#clr").hide();
+    $("#show").show();
 
-  });
-});
+}
